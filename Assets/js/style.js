67d4d29637f8498ba640bc9c4b84ba14bd85c255
Rafet Mammadov <professionalraf@gmail.com>
2021-11-13 23:18:51 +0400

    homework ended

--- Assets/js/style.js
+++ Assets/js/style.js
@@ -2,6 +2,8 @@ $('.owl-carousel').owlCarousel({
     margin:0,
     loop: true,
     nav:true,
+    dots:false,
+    autoplay:true,
     responsive:{
         0:{
             items:1
@@ -14,3 +16,34 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const myNav=document.querySelector('nav')
+const myLink =myNav.querySelectorAll('a')
+function darkMode(){
+    var elements = document.getElementsByClassName("arxasi-qaralacaq-elementler")
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle("back-black")
+    }
+    var whitetxt=document.getElementsByClassName('white-text')
+    for (let a = 0; a < whitetxt.length; a++) {
+        whitetxt[a].classList.toggle('txt-white')
+        
+    }
+    let slidebtn=document.getElementsByClassName('owl-next')
+    slidebtn[0].classList.toggle('btn-bg')
+    let slidebtn1=document.getElementsByClassName('owl-prev')
+    slidebtn1[0].classList.toggle('btn-bg')
+
+    let dark=document.getElementById('small-images')
+    dark.classList.toggle('sm-img-dark')
+
+    let cardfootdark=document.getElementsByClassName('card-foot-dark')
+
+    for (let a = 0; a < cardfootdark.length; a++) {
+        cardfootdark[a].classList.toggle('card-f-dark')
+        
+    }
+
+
+   
+}
